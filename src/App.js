6beb9fd9c1e3045d25a90e2a44af0ff02f146b03
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-19 08:39:06 +0200

    changed app to small letters.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import HomepageComponent from "./components/pages/homepage/homepage.component";
 
-function App() {
+function app() {
     return (
         <div>
             <HomepageComponent/>
@@ -9,4 +9,4 @@ function App() {
     );
 }
 
-export default App;
+export default app;
