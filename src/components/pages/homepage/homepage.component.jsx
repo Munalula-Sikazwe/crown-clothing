f88cf82a6cc2bfc8dcffed34516bc8f0f2ac959f
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-19 09:31:21 +0200

    changed the directory component.

--- src/components/pages/homepage/homepage.component.jsx
+++ src/components/pages/homepage/homepage.component.jsx
@@ -1,58 +1,11 @@
 import React from 'react';
 import './homepage.styles.scss';
-import MenuItemComponent from "../menu-item/menu-item.component";
+import DirectoryComponent from "../directory/directory.component";
+
 
 const homepageComponent = () => (
     <div className={"homepage"}>
-        <div className={"directory-menu"}>
-
-            <MenuItemComponent title={"HATS"}/>
-            <div className={"menu-item"}>
-                <div className={"content"}>
-                    <h1 className={"title"}>
-                        JACKETS
-                    </h1>
-                    <span className={'subtitle'}>
-                    SHOP NOW
-                    </span>
-                </div>
-
-            </div>
-            <div className={"menu-item"}>
-                <div className={"content"}>
-                    <h1 className={"title"}>
-                        SNEAKERS
-                    </h1>
-                    <span className={'subtitle'}>
-                    SHOP NOW
-                    </span>
-                </div>
-
-            </div>
-            <div className={"menu-item"}>
-                <div className={"content"}>
-                    <h1 className={"title"}>
-                        WOMENS
-                    </h1>
-                    <span className={'subtitle'}>
-                    SHOP NOW
-                    </span>
-                </div>
-
-            </div>
-            <div className={"menu-item"}>
-                <div className={"content"}>
-                    <h1 className={"title"}>
-                        MENS
-                    </h1>
-                    <span className={'subtitle'}>
-                    SHOP NOW
-                    </span>
-                </div>
-
-            </div>
-        </div>
-
+        <DirectoryComponent/>
     </div>
 )
 
