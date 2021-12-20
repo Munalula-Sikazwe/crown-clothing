f59c291d13f97b6299d9c0dcdce098ca0a4d0647
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-20 17:15:25 +0200

    rendering collections in shop component.

--- src/components/shop/shop.component.jsx
+++ src/components/shop/shop.component.jsx
@@ -1,14 +1,22 @@
 import React,{Component} from "react";
 import SHOP_DATA from "./data";
+import PreviewCollectionComponent from "../preview-collection/preview-collection.component";
 class ShopComponent extends Component {
 
 state = {
     collections: SHOP_DATA
 }
-render = ()=>(
-    <div>
-        SHOP PAGE
-    </div>
-)
+render = ()=>{
+    const {collections} = this.state;
+    return (<div>
+        {collections.map((id,...otherCollectionProps) =>(
+            <div key={id}>
+               <PreviewCollectionComponent {...otherCollectionProps}/>
+            </div>
+        ))}
+    </div>)
+}
+
+
 }
 export default ShopComponent;
