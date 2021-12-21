e7c16e78e0f2d07622bc6fcfaa48e00d6931dc59
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-21 05:01:15 +0200

    added collection item to collection preview component.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import './collection-item.component.scss';
 
-const collectionItemComponent = ()=>(
-<div>
+const collectionItemComponent = ({id,name,price,imageUrl})=>(
+<div className={"collection-item"}>
+<div className={"image"}
+        style={{backgroundImage:`url(${imageUrl})`}}
+/>
+<div className={"collection-footer"}>
+    <span className={"name"}>{name}</span>
+    <span className={"price"}>{price}</span>
+</div>
 
 </div>
 );
--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import "./preview-collection.styles.scss";
-
+import CollectionItemComponent from "../collection-item/collection-item.component";
 const previewCollectionComponent = ({title,items})=>(
     <div className={"collection-preview"}>
         <h1 className={"title"}>
             {title.toUpperCase()}
         </h1>
         <div className={"preview"}>
-            {items.filter((item,idx) => idx < 4).map(item =>(
-                <div key={item.id}>
-                    {item.name}
-                </div>
+            {items.filter((item,idx) => idx < 4).map(({id,...otherCollectionProps}) =>(
+                <CollectionItemComponent key={id} {...otherCollectionProps}/>
             ))}
         </div>
     </div>
