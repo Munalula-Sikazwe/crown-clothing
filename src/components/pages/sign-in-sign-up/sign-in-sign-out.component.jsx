d408409679f8e54ec878b237322436a4482060e3
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-22 10:07:27 +0200

    added sign in component to the sign in sign out page.

--- src/components/pages/sign-in-sign-up/sign-in-sign-out.component.jsx
+++ src/components/pages/sign-in-sign-up/sign-in-sign-out.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './sign-in-sign-out.styles.scss';
-
+import SignInComponent from "../../sign-in/sign-in.component";
 const signInSignOutComponent = () => (
     <div className={"sign-in-sign-out"}>
-        SIGN IN
+        <SignInComponent/>
     </div>
 );
 export default signInSignOutComponent;
