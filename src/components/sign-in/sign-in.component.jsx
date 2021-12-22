007ac30d5db1028819b17b2cc18f13d45fbc7c54
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-22 09:53:35 +0200

    preventing default action on submit.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -6,12 +6,15 @@ class SignInComponent extends Component {
         email : '',
         password: '',
     }
+    handleSubmit = event =>{
+        event.preventDefault();
+    }
     render = ()=>{
         const {email,password} = this.state;
             return (<div className={"sign-in"}>
                 <h2> I Already have an account</h2>
                 <span>Sign in with your email and password.</span>
-                <form>
+                <form OnSubmit={this.handleSubmit}>
                     <input name={"email"} type="email" value={email} required/>
                     <input name={"password"} type={"password"} value={password} required/>
                     <input type={"submit"} value={"Submit Form"}/>
