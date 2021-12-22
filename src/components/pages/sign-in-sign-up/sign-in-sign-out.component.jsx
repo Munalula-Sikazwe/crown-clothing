12a61af2608d56a336bf35253d6f592ff34f034d
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-22 08:42:44 +0200

    completed sign in sign out component skeleton.

--- src/components/pages/sign-in-sign-up/sign-in-sign-out.component.jsx
+++ src/components/pages/sign-in-sign-up/sign-in-sign-out.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './sign-in-sign-out.styles.scss';
 
 const signInSignOutComponent = ()=>(
-    <div>
-
+    <div className={"sign-in-sign-out"}>
+SIGN IN
     </div>
 )
 export default signInSignOutComponent;
