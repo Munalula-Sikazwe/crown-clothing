754b29a381f7f253a46852d21b4d6ec895db0346
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-22 09:26:33 +0200

    refactored state values.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,12 +3,21 @@ import './sign-in.styles.scss';
 
 class SignInComponent extends Component {
     state = {
-
+        email : '',
+        password: '',
     }
-    render = ()=>(
-        <div>
-            SIGN
-        </div>
-    )
+    render = ()=>{
+        const {email,password} = this.state;
+            return (<div className={"sign-in"}>
+                <h2> I Already have an account</h2>
+                <span>Sign in with your email and password.</span>
+                <form>
+                    <input name={"email"} type="email" value={email} required/>
+                    <input name={"password"} type={"password"} value={password} required/>
+                </form>
+            </div>)
+    }
+
+
 }
 export default SignInComponent
