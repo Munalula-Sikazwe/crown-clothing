222245208395faae98862ca8202148b65eccb720
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-22 11:56:29 +0200

    rendering label text for form input.

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -4,10 +4,14 @@ import "./form-input.styles.scss";
 const formInputComponent = ({handleChange, label, ...otherFormInputProps}) => (
     <div className={"group"}>
         <input className={"form-input"} onChange={handleChange} {...otherFormInputProps}/>
+        {console.log(label)}
         {
             label ?
                 (
-                    <label className={`${otherFormInputProps.value.length ? 'shrink' : ''} for`}/>
+                    <label
+                        className={`${otherFormInputProps.value.length ? 'shrink' : ''} form-input-label`}>
+                        {label}
+                    </label>
                 )
                 : null
         }
