686ae28a90ee8e0c61980669b7e528269f5028e5
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-22 11:17:30 +0200

    created form input skeleton.

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,2 +1,10 @@
 import React from 'react';
 import "./form-input.styles.scss";
+
+const formInputComponent = ({handleChange,label,...otherFormInputProps})=>(
+    <div>
+
+    </div>
+)
+
+export default formInputComponent;
