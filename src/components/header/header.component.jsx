e42773a50e729932dca7f03352c95c4096cc4c99
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-22 07:11:08 +0200

    created header component skeleton.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,3 +1,11 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 import './header.styles.scss';
+
+const header = ()=> (
+    <div className={"header"}>
+
+    </div>
+)
+
+export default header;
