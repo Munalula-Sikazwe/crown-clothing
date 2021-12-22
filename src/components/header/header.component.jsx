18a40112db90e3f9c519b44b3a338ab25539b6cc
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-22 13:12:12 +0200

    added signin page link to navigation page.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,15 +3,15 @@ import {Link} from 'react-router-dom';
 import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/logo.svg';
 
-const header = ()=> (
+const header = () => (
     <div className={"header"}>
         <Link to={"/"} className={"logo-container"}>
             <Logo className={"logo"}/>
         </Link>
         <div className={"options"}>
             <Link to={"/shop"} className={"option"}>SHOP</Link>
-<Link to={"/contact"} className={"option"}>CONTACT</Link>
-
+            <Link to={"/contact"} className={"option"}>CONTACT</Link>
+            <Link to={"/signin"} className={"option"}>Sign In</Link>
         </div>
     </div>
 )
