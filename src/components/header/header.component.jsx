4aae60233b1972ba125dfc4474bea60af2c1fd72
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-23 16:15:58 +0200

    dynamically changing header due to user auth state.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/logo.svg';
+import {auth} from "../firebase/firebase.utils";
 
-const header = () => (
+const header = ({currentUser}) => (
     <div className={"header"}>
         <Link to={"/"} className={"logo-container"}>
             <Logo className={"logo"}/>
@@ -11,7 +12,13 @@ const header = () => (
         <div className={"options"}>
             <Link to={"/shop"} className={"option"}>SHOP</Link>
             <Link to={"/contact"} className={"option"}>CONTACT</Link>
-            <Link to={"/signin"} className={"option"}>SIGN IN</Link>
+            {
+                currentUser ?
+                    <div onClick={auth.signOut}>
+                        SIGN OUT
+                    </div>
+                    : <Link to={"/signin"} className={"option"}>SIGN IN</Link>
+            }
         </div>
     </div>
 )
