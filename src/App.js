b82d452ae0343b0a27a855eb11439305437710c0
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-23 15:50:21 +0200

    adding user auth state.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import {Route, Switch} from "react-router-dom";
 import ShopComponent from "./components/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInSignOutComponent from "./components/pages/sign-in-sign-up/sign-in-sign-out.component";
+import {auth} from "./components/firebase/firebase.utils";
+import {Component} from "react";
 
-function app() {
-    return (
+class App extends Component{
+    state ={
+        currentUser:null
+    }
+    render = ()=>(
         <div>
-            <Header/>
-            <Switch>
-                <Route path={"/"} component={HomepageComponent} exact/>
-                <Route path={"/shop"} component={ShopComponent} exact/>
-                <Route path={"/signin"} component={SignInSignOutComponent} exact/>
-            </Switch>
+        <Header/>
+        <Switch>
+            <Route path={"/"} component={HomepageComponent} exact/>
+            <Route path={"/shop"} component={ShopComponent} exact/>
+            <Route path={"/signin"} component={SignInSignOutComponent} exact/>
+        </Switch>
 
-        </div>
-    );
+    </div>)
+
 }
 
-export default app;
+export default App;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,34 +2,41 @@ import React, {Component} from 'react';
 import './sign-in.styles.scss';
 import FormInputComponent from "../form-input/form-input.component";
 import CustomButtonComponent from "../custom-button/custom-button.component";
+import {signinWithGoogle} from "../firebase/firebase.utils";
+
 class SignInComponent extends Component {
     state = {
-        email : '',
+        email: '',
         password: '',
     }
-    handleSubmit = event =>{
+    handleSubmit = event => {
         event.preventDefault();
-        this.setState({email:'',password:'',})
+        this.setState({email: '', password: '',})
     }
-    handleChange = event =>{
-        const {value,name } = event.target;
-        this.setState({[name]:value});
+    handleChange = event => {
+        const {value, name} = event.target;
+        this.setState({[name]: value});
     }
-    render = ()=>{
-        const {email,password} = this.state;
-            return (<div className={"sign-in"}>
-                <h2> I Already have an account</h2>
-                <span>Sign in with your email and password.</span>
-                <form onSubmit={this.handleSubmit}>
-                    <FormInputComponent name={"email"} type={"email"} value={email} handleChange={this.handleChange} label={'email'} required/>
-                    <FormInputComponent name={"password"} type={"password"} value={password} handleChange={this.handleChange} label={'password'} required/>
-                    <CustomButtonComponent type={"submit"} value={"Submit Form"} >
-                        Sign In
-                    </CustomButtonComponent>
-                </form>
-            </div>)
+    render = () => {
+        const {email, password} = this.state;
+        return (<div className={"sign-in"}>
+            <h2> I Already have an account</h2>
+            <span>Sign in with your email and password.</span>
+            <form onSubmit={this.handleSubmit}>
+                <FormInputComponent name={"email"} type={"email"} value={email} handleChange={this.handleChange}
+                                    label={'email'} required/>
+                <FormInputComponent name={"password"} type={"password"} value={password}
+                                    handleChange={this.handleChange} label={'password'} required/>
+                <CustomButtonComponent type={"submit"}>
+                    Sign In
+                </CustomButtonComponent>
+
+                <CustomButtonComponent onClick={signinWithGoogle}>{'  '}Sign In with Google{' '}</CustomButtonComponent>
+            </form>
+        </div>)
     }
 
 
 }
+
 export default SignInComponent
