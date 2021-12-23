9c014910e2a2992b45cef90472af19c3b84ebcbb
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-23 17:05:36 +0200

    dynamically rendering sign in button class.

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './custom-button.styles.scss';
 
-const customButtonComponent = ({children,...otherButtonProps})=>(
-    <button className={"custom-button"} {...otherButtonProps}>
+const customButtonComponent = ({children,isGoogleSignIn,...otherButtonProps})=>(
+    <button className={`${isGoogleSignIn ? 'google-sign-in':''} custom-button`} {...otherButtonProps}>
         {children}
     </button>
 )
