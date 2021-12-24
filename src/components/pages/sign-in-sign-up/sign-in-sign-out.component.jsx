49eadd63ee519771a60f3f4202e7e5fe29d30167
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-24 14:36:28 +0200

    changed class name to match styles.

--- src/components/pages/sign-in-sign-up/sign-in-sign-out.component.jsx
+++ src/components/pages/sign-in-sign-up/sign-in-sign-out.component.jsx
@@ -3,7 +3,7 @@ import './sign-in-sign-out.styles.scss';
 import SignInComponent from "../../sign-in/sign-in.component";
 import SignUpComponent from "../../sign-up/sign-up.component";
 const signInSignOutComponent = () => (
-    <div className={"sign-in-sign-up"}>
+    <div className={"sign-in-and-sign-up"}>
         <SignInComponent/>
         <SignUpComponent/>
     </div>
