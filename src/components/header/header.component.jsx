7f469ba909a7b008f9efd9aa5b1f2bea46058ce3
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-25 18:17:32 +0200

    used the  connectadvanced on the header component.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/logo.svg';
 import {auth} from "../../firebase/firebase.utils";
-
+import {connectAdvanced} from "react-redux";
 const header = ({currentUser}) => (
     <div className={"header"}>
         <Link to={"/"} className={"logo-container"}>
@@ -23,4 +23,4 @@ const header = ({currentUser}) => (
     </div>
 )
 
-export default header;
+export default connectAdvanced()(header);
