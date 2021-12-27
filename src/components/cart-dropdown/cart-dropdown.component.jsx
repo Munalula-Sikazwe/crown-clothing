bf1e199dd3789b746894c1050dc7e1ed8a60272c
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-27 15:13:51 +0200

    added cart dropdown styles.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import CustomButtonComponent from "../custom-button/custom-button.component";
 import './cart-dropdown.styles.scss';
 
-const cartDropdownComponent = ()=>(
+const cartDropdownComponent = () => (
     <div className="cart-dropdown">
-<div className="cart-items">
-    <CustomButtonComponent>
-        GO TO CHECKOUT
-    </CustomButtonComponent>
-</div>
+        <div className="cart-items"/>
+        <CustomButtonComponent>
+            GO TO CHECKOUT
+        </CustomButtonComponent>
     </div>
 );
 
