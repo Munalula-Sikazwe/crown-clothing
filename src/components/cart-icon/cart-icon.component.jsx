2184f0569d06c86f0f1610dc08e6af918c67694f
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-27 11:12:40 +0200

    created cart icon files and directories and styles.

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './cart-icon.styles.scss';
+
+import {ReactComponent as ShoppingIcon} from "*.svg";
+
+const cartIcon = ()=>(
+    <div className="cart-icon">
+        <ShoppingIcon className={"shopping-icon"}/>
+        <span className="item-count">
+            0
+        </span>
+    </div>
+
+);
+export default cartIcon;
