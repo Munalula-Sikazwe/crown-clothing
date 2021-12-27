1f9ec9e46c825b29db33009afd8d7668b1d1178b
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-27 14:44:05 +0200

    created cart-dropdown component scarfolding.

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CustomButtonComponent from "../custom-button/custom-button.component";
+import './cart-dropdown.styles.scss';
+
+const cartDropdownComponent = ()=>(
+    <div className="cart-dropdown">
+
+    </div>
+);
+
+export default cartDropdownComponent;
