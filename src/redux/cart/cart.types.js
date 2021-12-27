91aafc6a2b32b27622461cbf98e1b01ec8fe6eb6
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-27 15:50:13 +0200

    created cart action type toggle hidden.

---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,11 @@
+const INITIAL_STATE = {
+    hidden : true
+}
+
+const cartReducer =(state=INITIAL_STATE,action)=>{
+
+    switch(action.type){
+        case  CartActionTypes.TOGGLE_CART_HIDDEN:
+
+    }
+}
