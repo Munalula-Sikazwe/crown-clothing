b61566f80e0be9e3cc1a0d81e67522223783f005
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-29 01:16:16 +0200

    removed redundant import.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,7 +3,6 @@ import './cart-icon.styles.scss';
 import {toggleCartHidden} from "../../redux/cart/cart.actions";
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import {connect} from "react-redux";
-import cartItem from "../cart-item/cart-item.component";
 import {selectCartItemsCount} from "../../redux/cart/cart.selectors";
 
 const cartIcon = ({toggleCartHidden,itemCount})=>(
