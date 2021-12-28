03b084948c396cb9c1cb7b39f66d649389bd9055
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-28 12:55:19 +0200

    fixed error caused by import refactor.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './cart-icon.styles.scss';
-import toggleCartHidden from "../../redux/cart/cart.actions";
+import {toggleCartHidden} from "../../redux/cart/cart.actions";
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import {connect} from "react-redux";
 
