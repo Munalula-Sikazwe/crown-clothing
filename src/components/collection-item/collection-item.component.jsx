10f72fc8578ac396366580840d102ddf135fef8f
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-28 13:19:40 +0200

    refactored collection component to allow the passing in of the item.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -4,20 +4,24 @@ import CustomButtonComponent from "../custom-button/custom-button.component";
 import {connect} from "react-redux";
 import {addItem} from "../../redux/cart/cart.actions";
 
-const collectionItemComponent = ({id,name,price,imageUrl})=>(
-<div className={"collection-item"}>
-<div className={"image"}
-        style={{backgroundImage:`url(${imageUrl})`}}
-/>
-<div className={"collection-footer"}>
-    <span className={"name"}>{name}</span>
-    <span className={"price"}>{price}</span>
-</div>
-    <CustomButtonComponent>
-        Add to Cart
-    </CustomButtonComponent>
-</div>
-);
+const collectionItemComponent = ({item,addItem})=>{
+    const {name,price,imageUrl} = item;
+    return(
+        <div className={"collection-item"}>
+            <div className={"image"}
+                 style={{backgroundImage:`url(${imageUrl})`}}
+            />
+            <div className={"collection-footer"}>
+                <span className={"name"}>{name}</span>
+                <span className={"price"}>{price}</span>
+            </div>
+            <CustomButtonComponent>
+                Add to Cart
+            </CustomButtonComponent>
+        </div>
+    );
+}
+
 const mapDispatchToProps = dispatch => (
     {
         addItem: item => dispatch(addItem(item))
