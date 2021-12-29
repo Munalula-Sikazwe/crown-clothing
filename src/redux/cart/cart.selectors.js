6a4f551038527f38d96df28b96df86174beb7828
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-29 16:51:14 +0200

    create cart total selector.

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -14,4 +14,9 @@ export const selectCartItems = createSelector(
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems => cartItems.reduce((total, cartItem)=> total + cartItem.quantity,0)
+);
+
+export const  selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce((total,cartItem) => total + cartItem.price,0)
 )
