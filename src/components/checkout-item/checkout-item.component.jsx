032499b35fc8ff5e68129782cd73fc4af03aa75d
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-30 18:40:26 +0200

    added the clear item feature to the checkout item.

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import './checkout-item.styles.scss';
-
-const checkoutItem = ({cartItem:{name,imageUrl,quantity,price}}) => (
-    <div className={"checkout-item"}>
-        <div className="image-container">
-            <img src={imageUrl} alt={"item"}/>
-        </div>
-        <span className="name">
+import {clearCart} from "../../redux/cart/cart.actions";
+import {connect} from "react-redux";
+import cartItem from "../cart-item/cart-item.component";
+const checkoutItem = ({cartItem,clearCart}) => {
+    const handleClear = ()=>clearCart(cartItem);
+    const {name,imageUrl,quantity,price} =cartItem;
+    return (
+        <div className={"checkout-item"}>
+            <div className="image-container">
+                <img src={imageUrl} alt={"item"}/>
+            </div>
+            <span className="name">
 {name}
         </span>
-        <span className="quantity">
+            <span className="quantity">
 {quantity}
         </span>
-        <span className="price">
+            <span className="price">
 {price}
         </span>
-        <div className="remove-button">
-            &#10005;
+            <div className="remove-button" onClick={handleClear}>
+                &#10005;
+            </div>
         </div>
-    </div>
-);
-
-export default checkoutItem;
+    );
+}
+const mapDispatchToProps = dispatch=>({
+    clearCart: item=> dispatch(clearCart(cartItem))
+});
+export default connect(null,mapDispatchToProps)(checkoutItem);
