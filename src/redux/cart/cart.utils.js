c81af6a34e0ae65947012ea6c3d33b616b0dfa86
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-30 19:56:00 +0200

    created remove item from cart utility function.

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,13 +1,27 @@
-export const addItemsToCart = (cartItems,cartItemToAdd)=>{
+export const addItemsToCart = (cartItems, cartItemToAdd) => {
 
-    const existingcartItem = cartItems.find( cartItem=> cartItem.id === cartItemToAdd.id);
-    if (existingcartItem){
+    const existingcartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
+    if (existingcartItem) {
         return cartItems.map(
             cartItem => cartItem.id === cartItemToAdd.id ?
-                {...cartItem,quantity: cartItem.quantity + 1}
+                {...cartItem, quantity: cartItem.quantity + 1}
                 : cartItem
         );
 
     }
-    return [...cartItems, {...cartItemToAdd,quantity: 1}]
+    return [...cartItems, {...cartItemToAdd, quantity: 1}]
+}
+
+export const removeItemsFromCart = (cartItems, cartItemToRemove) => {
+    const existingcartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
+    if (existingcartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    } else {
+        return cartItems.map(
+            cartItem => cartItem.id === cartItemToRemove.id ?
+                {...cartItem, quantity: cartItem.quantity - 1}
+                : cartItem
+        );
+
+    }
 }
