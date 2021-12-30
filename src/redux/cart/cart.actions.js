ab9dd3362981f6808cd426a9a5d6c84f424e172f
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-30 18:21:01 +0200

    added clear Item action.

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,13 +1,17 @@
 import {CartActionTypes} from "./cart.types";
 
-export const toggleCartHidden = ()=>({
-    type : CartActionTypes.TOGGLE_CART_HIDDEN
+export const toggleCartHidden = () => ({
+    type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
 
-export const addItem = item=>(
+export const addItem = item => (
     {
-        type:CartActionTypes.ADD_ITEM,
+        type: CartActionTypes.ADD_ITEM,
         payload: item
     }
 );
 
+export const clearCart = item => ({
+    type:CartActionTypes.CLEAR_ITEM,
+        payload:item
+});
