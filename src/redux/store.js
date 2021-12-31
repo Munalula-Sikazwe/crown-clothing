81859c930bd55dd610daea5dc387418d70b26a36
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-31 17:21:19 +0200

    configured a persisted store.

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,10 @@ import {createStore,applyMiddleware} from "redux";
 import {logger} from "redux-logger/src";
 
 import rootReducer from "./root-reducer";
+import {persistStore} from "redux-persist";
 
 const middlewares = [logger];
 
 const store = createStore(rootReducer,applyMiddleware(...middlewares));
-
-export default store;
+const persistor = persistStore(store)
+export default {store,persistor};
