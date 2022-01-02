863cc18364f5eaec0b1436a2a865dc8cfca45074
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-01-03 00:19:29 +0200

    removed redundant mappings.

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,12 +1,6 @@
 import {createSelector} from "reselect";
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
+
 const selectShop = state => state.shop;
 const selectCollections = createSelector(
     [selectShop],
@@ -19,7 +13,7 @@ export const selectCollection = collectionUrlParam => createSelector(
 
 export const selectCollectionsForPreview = createSelector(
     [selectCollections],
-    collections => Object.keys(collections).map(key=>collections[key])
+    collections => Object.keys(collections).map(key => collections[key])
 );
 
 export default selectCollections;
