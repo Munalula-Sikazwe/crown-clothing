a78cba82f6d6fe701a26535b1256b9dbea364fd8
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-01-02 15:24:49 +0200

    fixed typo in app.js

--- src/components/shop/shop.component.jsx
+++ src/components/shop/shop.component.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import CollectionsOverviewComponent from "../collections-overview/collections-overview.component";
 import {Route} from "react-router-dom";
-import CategoryComponent from "../collection/collection.component";
+import CollectionComponent from "../collection/collection.component";
 
 const shopComponent = ({match}) =>
     (<div className={"shop-page"}>
         <Route path={match.path} component={CollectionsOverviewComponent} exact/>
-        <Route path={`${match.path}/:categoryId`} component={CategoryComponent} exact/>
+        <Route path={`${match.path}/:categoryId`} component={CollectionComponent} exact/>
     </div>);
 export default shopComponent;
