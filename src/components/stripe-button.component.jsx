de44b508a3733fee2aa23cad457e45f80b51e3eb
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-01-02 21:08:26 +0200

    fixed error with the publishable key.

--- src/components/stripe-button.component.jsx
+++ src/components/stripe-button.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import StripeCheckout from "react-stripe-checkout";
-
-const stripeCheckoutButton = ({price})=>{
+const PUBLISHABLE_KEY = 'pk_test_51KDX8IBtMR7LvFCIibYrcvufNmjVgzXyU0wuQr0rj9AStcWwI5igZM1vbf0z73nBJ7NJlSKmdd2AnlW5y6MSpRuK009QTFT002';
+const stripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
-    const PUBLISHABLE_KEY = 'pk_test_51KDX8IBtMR7LvFCIibYrcvufNmjVgzXyU0wuQr0rj9AStcWwI5igZM1vbf0z73nBJ7NJlSKmdd2AnlW5y6MSpRuK009QTFT002'
-    return(
+
+    return (
         <StripeCheckout label={"Pay Now"}
                         name={'Crown Clothing Limited'}
-        image={"https://svgshare.com/i/CUz.svg"}
+                        image={"https://svgshare.com/i/CUz.svg"}
                         description={`Your total amount is $${price}`}
                         amount={priceForStripe}
                         panelLabel={"Pay Now"}
