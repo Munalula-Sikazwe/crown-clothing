28a5aafdf3389ca1d7eaae2f30d4a1ac0c730b77
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-01-02 21:08:46 +0200

    fixed error with the publishable key.

--- src/components/stripe-button.component.jsx
+++ src/components/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from "react-stripe-checkout";
+
 const PUBLISHABLE_KEY = 'pk_test_51KDX8IBtMR7LvFCIibYrcvufNmjVgzXyU0wuQr0rj9AStcWwI5igZM1vbf0z73nBJ7NJlSKmdd2AnlW5y6MSpRuK009QTFT002';
 const stripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
