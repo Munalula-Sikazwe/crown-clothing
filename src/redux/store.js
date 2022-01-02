648117bd25f22bc353500f3beb08da49811f9631
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-01-03 00:27:34 +0200

    fixed logger import

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import {createStore,applyMiddleware} from "redux";
-import {logger} from "redux-logger/src";
+import {logger} from "redux-logger";
 
 import rootReducer from "./root-reducer";
 import {persistStore} from "redux-persist";
