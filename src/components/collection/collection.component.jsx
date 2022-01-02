d781df495782e36d05bbf5d74c254e703b37ef7d
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-01-02 16:41:17 +0200

    completed collection component.

--- src/components/collection/collection.component.jsx
+++ src/components/collection/collection.component.jsx
@@ -2,18 +2,29 @@ import React from 'react';
 import './collection.styles.scss';
 import {selectCollection} from "../../redux/shop/shop.selectors";
 import {connect} from "react-redux";
-const collectionComponent = ({collection}) => (
+import CollectionItemComponent from "../collection-item/collection-item.component";
 
-    <div className="category">
-        <h2>
-            {console.log(collection)}
-            CATEGORY PAGE
+const collectionComponent = ({collection}) => {
+    const  {title,items} = collection;
+   return ( <div className="collection-page">
+        <h2 className={"title"}>
+            {title}
         </h2>
-    </div>
-);
-const mapStateToProps = (state,ownProps)=>(
+       <div className="items">
+           {
+               items.map(
+                   item => (
+                       <CollectionItemComponent key={item.id} item={item}/>
+                   )
+               )
+           }
+       </div>
+    </div>)
+}
+
+const mapStateToProps = (state, ownProps) => (
     {
-        collection:selectCollection(ownProps.match.params.collectionId)(state)
+        collection: selectCollection(ownProps.match.params.collectionId)(state)
     }
 );
 export default connect(mapStateToProps)(collectionComponent);
