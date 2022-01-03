482b0e6be9be3cdd2e18349dbe49df014a420849
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-01-03 18:58:42 +0200

    created logo container styles.

--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {Link} from "react-router-dom";
 
 export const HeaderContainer = styled.div
     `
@@ -7,4 +8,12 @@ export const HeaderContainer = styled.div
       display: flex;
       justify-content: space-between;
       margin-bottom: 25px;
+    `
+
+export const LogoContainer = styled(Link)
+    `
+      height: 100%;
+      width: 70px;
+      padding: 25px;
+        
     `
