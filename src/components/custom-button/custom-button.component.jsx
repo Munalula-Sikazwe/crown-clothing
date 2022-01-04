9e827d5e10d495f259520f57330006e81ec26615
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-01-04 18:50:29 +0200

    removed debug code from custom bug component.

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './custom-button.styles.scss';
 import {CustomButtonContainer} from "./custom-button.styles";
-const customButtonComponent = ({children, ...props})=>(
+
+const customButtonComponent = ({children, ...props}) => (
     <CustomButtonContainer {...props} >
-        {console.log(props.isGoogleSignIn)}
+
         {children}
     </CustomButtonContainer>
 )
