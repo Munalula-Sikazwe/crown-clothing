18605e95f195dbf29db16b0cf6fd97cd34306c8d
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-01-04 19:55:30 +0200

    converted shop component to a class component.

--- src/components/shop/shop.component.jsx
+++ src/components/shop/shop.component.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {Component} from "react";
 import CollectionsOverviewComponent from "../collections-overview/collections-overview.component";
 import {Route} from "react-router-dom";
 import CollectionComponent from "../collection/collection.component";
 
-const shopComponent = ({match}) =>
-    (<div className={"shop-page"}>
-        <Route path={match.path} component={CollectionsOverviewComponent} exact/>
-        <Route path={`${match.path}/:collectionId`} component={CollectionComponent} exact/>
-    </div>);
-export default shopComponent;
+class ShopComponent extends Component{
+
+    render = ()=> {
+        const {match} = this.props;
+        return (<div className={"shop-page"}>
+            <Route path={match.path} component={CollectionsOverviewComponent} exact/>
+            <Route path={`${match.path}/:collectionId`} component={CollectionComponent} exact/>
+        </div>);
+    }
+
+}
+
+export default ShopComponent;
