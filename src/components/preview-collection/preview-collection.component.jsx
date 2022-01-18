393399d77c172f4e50c0d3f1c03ca32a755380be
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-01-18 11:01:17 +0200

    added links to preview collection component . Links are designed using hooks.

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import "./preview-collection.styles.scss";
 import CollectionItemComponent from "../collection-item/collection-item.component";
-const previewCollectionComponent = ({title,items})=>(
-    <div className={"collection-preview"}>
-        <h1 className={"title"} >
-            {title.toUpperCase()}
-        </h1>
-        <div className={"preview"}>
-            {items.filter((item,idx) => idx < 4).map((item) =>(
+import {Link, useLocation} from "react-router-dom";
 
-                <CollectionItemComponent key={item.id} item={item}/>
-            ))}
+const PreviewCollectionComponent = ({title,items})=>{
+    const collectionLink = useLocation()
+    return(
+        <div className={"collection-preview"}>
+            <Link to={`${collectionLink.pathname}/${title.toLowerCase()}`}>
+                <h1 className={"title"} >
+                    {title.toUpperCase()}
+                </h1>
+            </Link>
+
+            <div className={"preview"}>
+                {items.filter((item,idx) => idx < 4).map((item) =>(
+
+                    <CollectionItemComponent key={item.id} item={item}/>
+                ))}
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
-export default previewCollectionComponent;
+
+export default PreviewCollectionComponent;
